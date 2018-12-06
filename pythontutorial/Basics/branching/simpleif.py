d4f066e7183ae22f81d517daa5847ff890f86bb1
DESKTOP-OKDL9BT\bhara <bharathiharsha>
2018-12-06 21:48:01 +0530

    Added python tutorials

---
+++ pythontutorial/Basics/branching/simpleif.py
@@ -0,0 +1,10 @@
+#whether a number is possitive
+n=eval(input("enter any no"))
+if n>0:
+    print(n, " is possitive")
+elif(n==0):
+    print(n, ' is neither negative nor possitive')
+else:
+    print(n,' is negative')
+
+print("exiting if")
